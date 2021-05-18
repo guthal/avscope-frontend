33ec0c66ac69d49fdb92781c92e7de5529f01cc1
guthal <37868861+guthal@users.noreply.github.com>
2021-05-18 22:15:34 +0530

    removed slashes2

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,23 +1,23 @@
 const DOMAIN = process.env.REACT_APP_DOMAIN;
 
 const ENDPOINTS = {
-  GET_CONTENT: (contentID) => `${DOMAIN}contents/${contentID}`,
-  GET_CONTENTS: `${DOMAIN}contents`,
-  GET_SERIES_CONTENTS: (seriesID) => `${DOMAIN}series/${seriesID}/contents`,
-  GET_SERIES_INFO: (seriesID) => `${DOMAIN}series/${seriesID}`,
-  GET_ALL_SERIES: `${DOMAIN}series`,
-  GET_HISTORY: (userID) => `${DOMAIN}history/${userID}`,
+  GET_CONTENT: (contentID) => `${DOMAIN}/contents/${contentID}`,
+  GET_CONTENTS: `${DOMAIN}/contents`,
+  GET_SERIES_CONTENTS: (seriesID) => `${DOMAIN}/series/${seriesID}/contents`,
+  GET_SERIES_INFO: (seriesID) => `${DOMAIN}/series/${seriesID}`,
+  GET_ALL_SERIES: `${DOMAIN}/series`,
+  GET_HISTORY: (userID) => `${DOMAIN}/history/${userID}`,
   GET_USER_CONTENT_PURCHASES: (userID, contentID) =>
-    `${DOMAIN}user-purchase/${userID}/contents/${contentID}`,
-  GET_USER_PURCHASE: (userID) => `${DOMAIN}user-purchase/${userID}`,
-  GET_CREATORS: `${DOMAIN}creators`,
-  POST_CONTENT_UPLOAD: `${DOMAIN}content-upload`,
-  POST_LOGIN: `${DOMAIN}login`,
-  GET_VERIFY_LOGIN: `${DOMAIN}login/verify`,
-  POST_SIGNUP: `${DOMAIN}register`,
-  GET_LOGOUT: `${DOMAIN}logout`,
-  POST_CREATE_ORDER_RAZORPAY: `${DOMAIN}orders/create`,
-  POST_ORDER_SUCCESS: `${DOMAIN}orders/success`,
+    `${DOMAIN}/user-purchase/${userID}/contents/${contentID}`,
+  GET_USER_PURCHASE: (userID) => `${DOMAIN}/user-purchase/${userID}`,
+  GET_CREATORS: `${DOMAIN}/creators`,
+  POST_CONTENT_UPLOAD: `${DOMAIN}/content-upload`,
+  POST_LOGIN: `${DOMAIN}/login`,
+  GET_VERIFY_LOGIN: `${DOMAIN}/login/verify`,
+  POST_SIGNUP: `${DOMAIN}/register`,
+  GET_LOGOUT: `${DOMAIN}/logout`,
+  POST_CREATE_ORDER_RAZORPAY: `${DOMAIN}/orders/create`,
+  POST_ORDER_SUCCESS: `${DOMAIN}/orders/success`,
 };
 
 export { ENDPOINTS };
